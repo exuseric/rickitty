99741c36caa82da44b8a2ca9cca53550dd65e2da
Eric Gathoni <ericbro29@gmail.com>
2024-02-19 15:26:07 +0300

    Update search bar component UI

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -1,19 +1,23 @@
 import { RiSearch2Line } from "react-icons/ri";
 
 export default function SearchBar() {
+
     return (
-        <form className="max-w-prose w-full p-2 my-6 flex items-center justify-between flex-wrap gap-2 border border-slate-200 rounded-sm">
+        <form className="max-w-prose w-full p-2 my-6 flex items-center justify-center flex-wrap gap-2 border border-slate-200 rounded-sm">
             <div className="flex items-center w-full">
                 <input type="search" name="search" id="search" className="bg-slate-50 border py-2 px-4 outline-primary w-full" required/>
             </div>
-            <div className="flex items-center justify-center gap-2 sm:self-center">
+            <div className="flex items-center justify-center gap-2 self-center">
                 <select id="categories" className="md:w-full">
                     <option className="block p-2" value="character" selected>Characters</option>
                     <option value="location">Locations</option>
                     <option value="episode">Episodes</option>
                 </select>
-                <button type="submit" className="btn--primary btn--icon-only btn--32 btn--round h-full" aria-label="search">
-                    <span className="icon text-white">
+                <button type="submit" className="btn--primary hover:btn--primary-hover btn--icon btn--md h-full" aria-label="search">
+                    <span className="text">
+                        Search 
+                    </span>
+                    <span className="icon text-inherit">
                         <RiSearch2Line aria-hidden="true" focusable="false"/>
                     </span>
                 </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-import SearchBar from "./components/search-bar";
+import SearchBar from "@/app/components/search-bar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center py-16">
+    <main className="flex min-h-screen flex-col items-center px-4 py-16">
       <header className="text-center">
         <h1 className="font-sans font-bold text-5xl text-bold text-black">Rickitty</h1>
         <p>Explore the world of rick and morty</p>
